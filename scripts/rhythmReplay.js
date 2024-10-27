c195456f12f4e65a5c9269d954f369e66d93a750
amhart02 <har20010@byui.edu>
2024-10-26 23:26:20 -0600

    Ensuring API works

--- scripts/rhythmReplay.js
+++ scripts/rhythmReplay.js
@@ -1,17 +1,15 @@
-const API_KEY = process.env.API_KEY; 
+// const API_KEY = ; 
+// once I figure out the whole .env file I can put the real API Key in here and github won't get mad at me
 const searchQuery = 'music';
-const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchQuery}&type=video&videoCategoryId=10&regionCode=US&relevanceLanguage=en&key=${API_KEY}`;
+const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchQuery}%20official%20music%20video&type=video&videoCategoryId=10&videoDuration=medium&safeSearch=strict&regionCode=US&relevanceLanguage=en&maxResults=50&key=${API_KEY}`;
 
 console.log("Script is working")
 fetch(url)
   .then(response => response.json())
   .then(data => {
-    // Optionally, filter for videos with titles that contain only English characters
-    const englishResults = data.items.filter(item => /^[a-zA-Z0-9\s.,'!?]+$/.test(item.snippet.title));
-    
-    // Print out the filtered video titles
-    englishResults.forEach(item => {
-      console.log(item.snippet.title);
+    const items = data.items;
+    items.forEach(item => {
+      console.log(item.snippet.title); // Print out the video titles
     });
   })
   .catch(error => {
