414c0563591de084f4a588a82091fe0ad2b1d024
amhart02 <har20010@byui.edu>
2024-11-21 10:24:38 -0700

    Made some changes

---
+++ scripts/catQuiz.mjs
@@ -0,0 +1,25 @@
+const catTrivia = [
+    {
+        title: "Sphynx",
+
+    },
+    {
+        title: "Siamese",
+
+    },
+    {
+        title: "Himalayan",
+
+    },
+    {
+        title: "Maine Coon",
+
+    },
+    {
+        title: "Cheetoh",
+
+    }
+]
+
+
+export default catTrivia;
