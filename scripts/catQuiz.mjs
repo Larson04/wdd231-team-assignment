7019194cdb8f65645bfd48f6b9862beb7e7a40ef
amhart02 <har20010@byui.edu>
2024-12-11 13:02:21 -0700

    I don't remember what I did haha

--- scripts/catQuiz.mjs
+++
@@ -1,9 +0,0 @@
-const catTrivia = [
-    { name: "Sphynx", imgURL: ""},
-    { name: "Siamese", imgURL: ""},
-    { name: "Himalayan", imgURL: ""},
-    { name: "Maine Coon", imgURL: ""},
-    { name: "Cheetoh", imgURL: ""}
-]
-
-export default catTrivia;
